a64bf7686ec86ca2be2dfdcf58d80ef42dd9e5cb
Naramazuki <mathisreine@outlook.com>
2022-10-19 10:56:27 +0200

    ajout de fonction

--- Game.java
+++ Game.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Game {
+    public void Menu(){
+        System.out.println("Game Menu");
+        System.out.println("appuyer sur une touche pour jouer");
+        Scanner sc=new Scanner(System.in);
+        String men=sc.next();
+        if(men!=""){
+            System.out.println("choisissez la race");
+        }
+        sc.close();
+    }
     public static void main(String[] args) {
         System.out.println("lancement du jeu, choisissez le nome de votre personnage ainsi que sa race");
         Scanner sc = new Scanner(System.in);
